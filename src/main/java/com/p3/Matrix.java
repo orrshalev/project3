55e04dafb2b9d5b604d0d703ee9e784d64c7cf7e
Orr Shalev <shalev.orr@gmail.com>
2022-02-22 11:01:23 -0500

    IURHTae

--- src/main/java/com/p3/Matrix.java
+++ src/main/java/com/p3/Matrix.java
@@ -1,12 +1,27 @@
 package com.p3;
 
 import java.util.LinkedList;
+import java.awt.Color;
 
 public class Matrix {
     // not implemented
     public static LinkedList<Line> applyTransformation(double[][] matrix, LinkedList<Line> lines) {
-        return new LinkedList<Line>();
-        
+        LinkedList<Line> newlines = new LinkedList<Line>();
+        lines.forEach(line -> {
+            int x0 = line.x0;
+            int y0 = line.y0;
+            int x1 = line.x1;
+            int y1 = line.x1;
+            Color color = line.color;
+            double[][] x0y0 = matrixMultiplication(coordinateMatrix(x0, y0), matrix);
+            double[][] x1y1 = matrixMultiplication(coordinateMatrix(x1, y1), matrix);
+            x0 = (int)x0y0[0][0];
+            y0 = (int)x0y0[0][1];
+            x1 = (int)x1y1[0][0];
+            y1 = (int)x1y1[0][1];
+            newlines.add(new Line(x0,y0,x1,y1,color));
+        });
+        return lines;
     }
 
     public static double[][] matrixMultiplication(double[][] A, double[][] B) {
